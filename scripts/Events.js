e7b4ae9d955b44a596f2e341650b2c4a380ed829
Kevin Kragenbrink <kevin@writh.net>
2017-09-22 03:33:37 -0700

    'prettier'

--- scripts/Events.js
+++ scripts/Events.js
@@ -1,38 +1,41 @@
-on('add:graphic', (obj) => {
-    const {token: token, represents: represents} = identifyToken(obj);
+on("add:graphic", obj => {
+  const { token: token, represents: represents } = identifyToken(obj);
 
-    if (isNPC(token)) {
-        trackNPCToken(token, represents);
-        setNPCTokenHP(token, represents);
-        selectTokenSide(token, represents);
-        setOrdinalTokenName(token, represents);
-    }
+  if (isNPC(token)) {
+    trackNPCToken(token, represents);
+    setNPCTokenHP(token, represents);
+    setTimeout(() => selectTokenSide(token, represents), NPC_SETUP_TIME);
+    setTimeout(() => setOrdinalTokenName(token, represents), NPC_SETUP_TIME);
+  }
 });
 
-on('change:graphic', (obj, prev) => {
-    if (!isObjectToken(obj)) return;
-    if (!hasObjectMoved(obj, prev)) return;
+on("change:graphic", (obj, prev) => {
+  if (!isObjectToken(obj)) return;
+  if (!hasObjectMoved(obj, prev)) return;
 
-    setTokenRotation(obj);
+  setTokenRotation(obj);
 
-    if (isFollower(obj.id)) unfollowToken(obj.id);
-    if (hasFollowers(obj.id)) state.writh.followers[obj.id].forEach((follower) => moveFollower(follower, obj.id));
+  if (isFollower(obj.id)) unfollowToken(obj.id);
+  if (hasFollowers(obj.id))
+    state.writh.followers[obj.id].forEach(follower =>
+      moveFollower(follower, obj.id)
+    );
 });
 
-on('chat:message', (evt) => {
-    if (evt.type === "api") processCommand(evt);
+on("chat:message", evt => {
+  if (evt.type === "api") processCommand(evt);
 });
 
-on('destroy:graphic', (obj) => {
-    const {token: token, represents: represents} = identifyToken(obj);
+on("destroy:graphic", obj => {
+  const { token: token, represents: represents } = identifyToken(obj);
 
-    if (isNPC(token)) {
-        untrackNPCToken(token, represents);
-    }
+  if (isNPC(token)) {
+    untrackNPCToken(token, represents);
+  }
 });
 
-on('ready', () => {
-    state.writh = state.writh || {};
-    state.writh.npcs = state.writh.npcs || {};
-    state.writh.followers = state.writh.followers || {};
+on("ready", () => {
+  state.writh = state.writh || {};
+  state.writh.npcs = state.writh.npcs || {};
+  state.writh.followers = state.writh.followers || {};
 });
--- scripts/NPCs.js
+++ scripts/NPCs.js
@@ -1,74 +1,79 @@
 const NPC_SETUP_TIME = 500;
 
 function setNPCTokenHP(token, represents) {
-	const hpformulaAttr = getSheetAttribute(represents, "npc_hpformula");
-	const conAttr = getSheetAttribute(represents, "npcd_con");
-	if (!hpformulaAttr || !conAttr) return;
-	const hpformula = hpformulaAttr.get("current");
-	const hpmod = Math.floor(parseInt(conAttr.get("current")) / 2) - 5;
-	let [hitDie, hitDieType] = hpformula.split("d");
-	if (hitDieType.indexOf("+") > -1) {
-		[hitDieType] = hitDieType.split("+");
-	}
-	if (hitDieType.indexOf("-") > -1) {
-		[hitDieType] = hitDieType.split("-");
-	}
-	let hp = 0;
-	for (let i = 0; i < +hitDie; i++) {
-		hp += randomInteger(+hitDieType) + hpmod;
-	}
+  const hpformulaAttr = getSheetAttribute(represents, "npc_hpformula");
+  const conAttr = getSheetAttribute(represents, "npcd_con");
+  if (!hpformulaAttr || !conAttr) return;
+  const hpformula = hpformulaAttr.get("current");
+  const hpmod = Math.floor(parseInt(conAttr.get("current")) / 2) - 5;
+  let [hitDie, hitDieType] = hpformula.split("d");
+  if (hitDieType.indexOf("+") > -1) {
+    [hitDieType] = hitDieType.split("+");
+  }
+  if (hitDieType.indexOf("-") > -1) {
+    [hitDieType] = hitDieType.split("-");
+  }
+  let hp = 0;
+  for (let i = 0; i < +hitDie; i++) {
+    hp += randomInteger(+hitDieType) + hpmod;
+  }
 
-	setTimeout(() => token.set("bar1_link", null), NPC_SETUP_TIME);
-	setTimeout(() => token.set("bar1_value", hp), NPC_SETUP_TIME);
-	setTimeout(() => token.set("bar1_max", hp), NPC_SETUP_TIME);
+  setTimeout(() => token.set("bar1_link", null), NPC_SETUP_TIME);
+  setTimeout(() => token.set("bar1_value", hp), NPC_SETUP_TIME);
+  setTimeout(() => token.set("bar1_max", hp), NPC_SETUP_TIME);
 }
 
-function resetOrdinals (evt) {
-	const tokens = getSelectedTokens(evt);
-	tokens.forEach((token) => {
-		const represents = token.get("represents");
-		delete state.writh.npcs[represents];
+function resetOrdinals(evt) {
+  const tokens = getSelectedTokens(evt);
+  tokens.forEach(token => {
+    const represents = token.get("represents");
+    delete state.writh.npcs[represents];
 
-		const nameAttr = getSheetAttribute(represents, "npc_name");
-		const name = nameAttr.get("current");
-		debug(`Resetting ordinals for ${name}.`);
-	});
+    const nameAttr = getSheetAttribute(represents, "npc_name");
+    const name = nameAttr.get("current");
+    debug(`Resetting ordinals for ${name}.`);
+  });
 }
 
 function trackNPCToken(token, represents) {
-	const list = state.writh.npcs[represents] || [];
-	list.push(token.id);
-	state.writh.npcs[represents] = list;
+  const list = state.writh.npcs[represents] || [];
+  list.push(token.id);
+  state.writh.npcs[represents] = list;
 }
 
 function untrackNPCToken(token, represents) {
-	const list = state.writh.npcs[represents] || [];
-	const idx = list.indexOf(token.id);
-	list.splice(idx, 1);
-	state.writh.npcs[represents] = list;
+  const list = state.writh.npcs[represents] || [];
+  const idx = list.indexOf(token.id);
+  list.splice(idx, 1);
+  state.writh.npcs[represents] = list;
 }
 
 function setOrdinalTokenName(token, represents) {
-	const list = state.writh.npcs[represents] || [];
-	const ord = getOrdinal(list.length);
-	const name = getTokenName(token, represents);
-	token.set("name", ord + " " + name);
+  const list = state.writh.npcs[represents] || [];
+  const ord = getOrdinal(list.length);
+  const name = getTokenName(token, represents);
+  token.set("name", ord + " " + name);
 }
 
 function selectTokenSide(token, represents) {
-	const name = getTokenName(token, represents);
-	const rollableTableObj = findObjs({
-		_type: "rollabletable",
-		name: name
-	})[0];
-	if (rollableTableObj == undefined) return;
-	const rollableTableItemsObj = findObjs({
-		_type: "tableitem",
-		_rollabletableid: rollableTableObj.get("_id")
-	});
-	if (rollableTableItemsObj.length < 1) return;
-	const side = Math.floor(Math.random() * (rollableTableItemsObj.length+1));
-	const src = rollableTableItemsObj[side].get("avatar").replace("/max", "/thumb");
-	token.set("currentSide", side);
-	token.set("imgsrc", src);
+  const name = getTokenName(token, represents);
+  const rollableTableObj = findObjs({
+    _type: "rollabletable",
+    name: name
+  })[0];
+  if (rollableTableObj == undefined) return;
+  const rollableTableItemsObj = findObjs({
+    _type: "tableitem",
+    _rollabletableid: rollableTableObj.get("_id")
+  });
+  if (rollableTableItemsObj.length < 1) return;
+  const side = Math.floor(Math.random() * rollableTableItemsObj.length);
+  debug(`name: ${name}, side: ${side}`);
+  setTimeout(() => {
+    const src = rollableTableItemsObj[side]
+      .get("avatar")
+      .replace("/max", "/thumb");
+    token.set("currentSide", side);
+    token.set("imgsrc", src);
+  }, NPC_SETUP_TIME);
 }
